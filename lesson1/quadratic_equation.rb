8396e4f92f67a1f868447c09bd36ef82f0411495
enextus <enextus@gmail.com>
2018-03-14 16:44:31 +0300

    removed unnecessary brackets

--- lesson1/quadratic_equation.rb
+++ lesson1/quadratic_equation.rb
@@ -12,18 +12,18 @@ print 'Пожалуйста введите коэффициент (c): '
 c = gets.to_f
 
 # вычисляем дискриминант
-d = (b**2) - (4 * a * c)
+d = b**2 - 4 * a * c
 
 if d > 0
   sqrt_of_d = Math.sqrt(d)
-  x1 = (-b + sqrt_of_d) / (2 * a)
-  x2 = (-b - sqrt_of_d) / (2 * a)
+  x1 = -b + sqrt_of_d / 2 * a
+  x2 = -b - sqrt_of_d / 2 * a
   puts "Дискриминанат = #{d},"
   puts "1-й корень = #{x1},"
   puts "2-й корень = #{x2}."
 elsif d.zero?
   puts "Дискриминанат = #{d},"
-  x = -b / (2 * a)
+  x = -b / 2 * a
   puts "Oба корня имееют одно значение = #{x}."
 else
   puts 'Корней нет'
