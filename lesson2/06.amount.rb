bd20dd06ba2f810b0d568baea442e6d74f0d22d2
enextus <enextus@gmail.com>
2018-03-18 22:21:05 +0300

    ammount.rb is ready

---
+++ lesson2/06.amount.rb
@@ -0,0 +1,28 @@
+# Программа вычисления суммы покупок
+# hash_main = {[:name], [:price price, :amount amount]}
+hash_names = {}
+sum = 0
+loop do
+  print 'Пожалуйста, введите название товара ("стоп" для окончания): '
+  name = gets.chomp
+
+  break if name == 'стоп'
+
+  print 'Пожалуйста, введите цену за единицу товара: '
+  price = gets.to_f
+
+  print 'Пожалуйста, введите количество единиц: '
+  amount = gets.to_i
+  hash_names[:"#{name}"] = [price: price, amount: amount]
+end
+puts '--------------------------------------------------------------------- '
+puts 'Вывод хеша, имя товара, цена за единицу товара и кол-во купл. товара: '
+puts hash_names
+hash_names.each_with_index do |(name, value), index|
+  print "Итоговая сумма за товар по названию '#{name}' составляет: "
+    value.each do |couple|
+    puts bill = (couple[:price] * couple[:amount]).round(2)
+    sum += bill
+  end
+  puts "Итоговая сумма за все товары #{sum.round(2)}" if (index + 1).equal?hash_names.length
+end
