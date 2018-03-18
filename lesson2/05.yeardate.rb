15b37ce6668cd41aadc5ea9afe16c369af3e91dc
enextus <enextus@gmail.com>
2018-03-18 10:05:10 +0300

    yeardate

--- lesson2/05.yeardate.rb
+++ lesson2/05.yeardate.rb
@@ -9,22 +9,19 @@ month = gets.to_i
 print 'Пожалуйста введите год: '
 year = gets.to_i
 
-def leapyear(year)
-  if (year % 4.zero? && year % 100 != 0) || (year % 400.zero?)
-    @arr = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  else
-    @arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  end
-end
-
 def whole_d(year, day, month)
-  year = year
-  x = 0
+  arr = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+  # коррекция значения элемента в зависимости от високосности года
+  arr[1] = 29 if ((year % 4).zero? && year % 100 != 0) || (year % 400).zero?
+
+  i = 0
   result = 0
-  leapyear(year)
-  @arr.inject(0) do |sum, i|
-    x += 1
-    result = sum + i if x < month
+
+  arr.inject(0) do |sum, index|
+    i += 1
+    puts "sum = #{sum} index = #{index}"
+    result = sum + index if i < month
   end
   puts "Количество дней = #{result + day}"
 end
