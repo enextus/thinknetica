50bb0157a07b0a96ee19bb4582a257458a064c91
enextus <enextus@gmail.com>
2018-03-20 10:34:16 +0300

    fib update

--- lesson2/03.fib.rb
+++ lesson2/03.fib.rb
@@ -1,21 +1,12 @@
 # Программа наполнения массива от 1 до 100 числами Фибоначчи
 # M[n] = fib(n - 1) + fib(n - 2)
 
-arr = []
+  fibbonacci = [0, 1]
+  next_element = 1
 
-def fib(arr)
-  a = 0
-  b = 1
-
-  i = 0
-  loop do
-    a, b = b, a + b
-    break if a >= 100
-    arr[i] = a
-    i += 1
+  while next_element < 100
+    fibbonacci << next_element
+    next_element = fibbonacci[-1] + fibbonacci[-2]
   end
-end
 
-fib(arr)
-
-arr.each { |f| puts f }
+  p fibbonacci
