567c37ebfde8025cda36581882818a955dbc3c32
enextus <enextus@gmail.com>
2018-04-10 17:22:11 +0300

    10.04 oop2 005

--- 04.lesson/train.rb
+++ 04.lesson/train.rb
@@ -3,6 +3,8 @@
 # class Train
 class Train
 
+  public
+
   attr_reader :train_number, :wagons, :type, :speed, :route
 
   def initialize(train_number, wagons, type)
@@ -12,43 +14,9 @@ class Train
     @speed = 0
   end
 
-  def accelerate(value)
-    return unless value.positive?
-    @speed += value
-  end
-
-  def decelerate(value)
-    return if value.negative? || @speed < value
-    @speed -= value
-  end
-
-  def add_wagon
-    return unless speed.zero?
-    @wagons += 1
-  end
-
-  def delete_wagon
-    return unless speed.zero? || wagons.zero?
-    @wagons -= 1
-  end
-
   def receive_route(route)
     @route = route
     @index = 0
-    current_station.arrive(self)
-  end
-
-  def move_train_forward
-    return if @route.stations.nil? || @index >= @route.stations.size - 1
-    current_station.depart(self)
-    @index += 1
-    current_station.arrive(self)
-  end
-
-  def move_train_backward
-    return if @route.stations.nil? || @index.zero?
-    current_station.depart(self)
-    @index -= 1
     current_station.arrive(self)
   end
 
@@ -65,6 +33,48 @@ class Train
     return if @index >= @route.stations.size - 1
     @route.stations[@index + 1]
   end
+
+  protected
+
+  # в ТЗ не требуется ни вывод ни возвращать
+  def move_train_forward
+    return if @route.stations.nil? || @index >= @route.stations.size - 1
+    current_station.depart(self)
+    @index += 1
+    current_station.arrive(self)
+  end
+
+  # в ТЗ не требуется ни вывод ни возвращать
+  def move_train_backward
+    return if @route.stations.nil? || @index.zero?
+    current_station.depart(self)
+    @index -= 1
+    current_station.arrive(self)
+  end
+
+  # Может возвращать текущую скорость - в ТЗ не требуется ни вывод ни возвращать
+  def accelerate(value)
+    return unless value.positive?
+    @speed += value
+  end
+
+  # Может тормозить - в ТЗ не требуется ни выводить ни возвращать
+  def decelerate(value)
+    return if value.negative? || @speed < value
+    @speed -= value
+  end
+
+  # в ТЗ не требуется ни выводить ни возвращать
+  def add_wagon
+    return unless speed.zero?
+    @wagons += 1
+  end
+
+  # в ТЗ не требуется ни выводить ни возвращать
+  def delete_wagon
+    return unless speed.zero? || wagons.zero?
+    @wagons -= 1
+  end
 end
 
 # class PassengerTrain
@@ -74,8 +84,6 @@ class PassengerTrain < Train
     # вызываешь супер, передаешь аргументы, инициализируешь в родителе
     super(train_number, wagons, 'passenger')
   end
-
-
 end
 
 # class CargoTrain
@@ -84,5 +92,9 @@ class CargoTrain < Train
   def initialize(train_number, wagons)
     # вызываешь супер, передаешь аргументы, инициализируешь в родителе
     super(train_number, wagons, 'cargo')
+  end
+
+  def hallo(value)
+    accelerate(value)
   end
 end
