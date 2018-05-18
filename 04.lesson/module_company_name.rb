b28237579122cca72737dee4697da177d45974c0
enextus <enextus@gmail.com>
2018-05-18 15:58:37 +0300

    module accessor only

--- 04.lesson/module_company_name.rb
+++ 04.lesson/module_company_name.rb
@@ -1,12 +1,3 @@
   module CompanyName
-    def set_company_name(name)
-      @company_name = name
-    end
-
-    def get_company_name
-      @company_name
-    end
-
-    protected
     attr_accessor :company_name
   end
