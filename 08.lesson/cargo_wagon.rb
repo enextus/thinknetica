cd2691d26314f5adef58494cbcd7d1ad177ca6fb
enextus <enextus@gmail.com>
2018-05-29 15:21:00 +0300

    modifed cargo_wagon passenger_wagon

--- 08.lesson/cargo_wagon.rb
+++ 08.lesson/cargo_wagon.rb
@@ -3,7 +3,25 @@
 
 # class CargoWagon
 class CargoWagon < Wagon
-  def initialize
+  attr_reader :volume_capacity
+
+  def initialize(volume_capacity)
     super('cargo')
+    @volume_capacity = volume_capacity
+    @free_volume_amount = @volume_capacity if @volume_capacity.positive?
+    @free_volume_amount ||= 0
+  end
+
+  def charge_volume_amount(amount)
+    return if amount > @free_volume_amount || @free_volume_amount.zero?
+    @free_volume_amount -= amount
+  end
+
+  def how_much_volume_was_charged
+    @volume_capacity - @free_volume_amount
+  end
+
+  def free_volume_amount
+    @volume_capacity - how_much_volume_was_charged
   end
 end
--- 08.lesson/passenger_wagon.rb
+++ 08.lesson/passenger_wagon.rb
@@ -3,7 +3,25 @@
 
 # class CargoWagon
 class PassengerWagon < Wagon
-  def initialize
+  attr_reader :place_capacity
+
+  def initialize(place_capacity)
     super('passenger')
+    @place_capacity = place_capacity
+    @free_places_amount = @place_capacity if @place_capacity.positive?
+    @free_places_amount ||= 0
+  end
+
+  def booking_single_place
+    return if @free_places_amount.zero?
+    @free_places_amount -= 1
+  end
+
+  def how_places_are_booked
+    @place_capacity - @free_places_amount
+  end
+
+  def free_places_amount
+    @place_capacity - how_places_are_booked
   end
 end
