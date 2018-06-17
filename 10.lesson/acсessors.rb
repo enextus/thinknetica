d22abb3fe6be4949acd1bbf874654e80cd3d6e84
enextus <enextus@gmail.com>
2018-06-17 11:00:21 +0300

    modiefed module Accessors

--- 10.lesson/acсessors.rb
+++ 10.lesson/acсessors.rb
@@ -5,17 +5,24 @@ module Accessors
   def attr_accessor_with_history(*names)
     names.each do |name|
       var_name = "@#{name}".to_sym
-
       define_method(name) { instance_variable_get(var_name) }
-
+      arr = []
       define_method("#{name}=".to_sym) do |value|
         instance_variable_set(var_name, value)
-
-        @arr << value
-        @history[var_name] = @arr
-
-        define_singleton_method("#{name}_history".to_sym) { @arr }
+        define_singleton_method("#{name}_history".to_sym) { @history[name.to_sym] }
+        arr << value
+        @history[name.to_sym] = arr
       end
+    end
+  end
+
+  def strong_attr_accessor(a_name, a_class)
+
+    var_name = "@#{a_name}".to_sym
+    define_method(a_name) { instance_variable_get(var_name) }
+
+    define_method("#{a_name}=".to_sym) do |value|
+      instance_variable_set(var_name, value)
     end
   end
 end
@@ -26,8 +33,11 @@ class Test
 
   def initialize
     @history = {}
-    @arr = []
   end
 
+  attr_reader :history
+
   attr_accessor_with_history :my_attr, :a, :b, :c
+
+  strong_attr_accessor :d, String
 end
