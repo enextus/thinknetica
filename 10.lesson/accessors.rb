38ac46c9fa6d1bc057dea876086f8e956adf2fe9
enextus <enextus@gmail.com>
2018-06-19 00:12:23 +0300

    modified history and raise accessors.rb

--- 10.lesson/accessors.rb
+++ 10.lesson/accessors.rb
@@ -2,18 +2,19 @@
 
 # module Accessors
 module Accessors
-  @@history = {}
-
   def attr_accessor_with_history(*names)
     names.each do |name|
       var_name = "@#{name}".to_sym
-      define_method(name) { instance_variable_get(var_name) }
-      arr = []
+      var_name_history = "@#{name}_history".to_sym
+
+      define_method(name.to_sym) { instance_variable_get(var_name) }
+      define_method("#{name}_history".to_sym) { instance_variable_get(var_name_history) }
+
       define_method("#{name}=".to_sym) do |value|
         instance_variable_set(var_name, value)
-        define_singleton_method("#{name}_history".to_sym) { @@history[name.to_sym] }
-        arr << value
-        @@history[name.to_sym] = arr
+        history = instance_variable_get(var_name_history)
+        history.nil? ? history = [value] : history << value
+        instance_variable_set(var_name_history, history)
       end
     end
   end
@@ -22,8 +23,8 @@ module Accessors
     var_name = "@#{name}".to_sym
     define_method(name.to_sym) { instance_variable_get(var_name) }
     define_method("#{name}=".to_sym) do |value|
-      instance_variable_set(var_name, value) if value.class == type
       raise TypeError, 'Wrong type argument!' if value.class != type
+      instance_variable_set(var_name, value)
     end
   end
 end
