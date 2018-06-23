fd205c49321f4192089a6dab2b413b9fcbb75c3c
enextus <enextus@gmail.com>
2018-06-23 15:07:26 +0300

    modified accessors.rb history

--- 10.lesson/accessors.rb
+++ 10.lesson/accessors.rb
@@ -5,16 +5,17 @@ module Accessors
   def attr_accessor_with_history(*names)
     names.each do |name|
       var_name = "@#{name}".to_sym
-      var_name_history = "@#{name}_history".to_sym
+      var_history = "@#{name}_history".to_sym
       history = []
 
       define_method(name.to_sym) { instance_variable_get(var_name) }
-      define_method("#{name}_history".to_sym) { instance_variable_get(var_name_history) }
+      define_method("#{name}_history".to_sym) { instance_variable_get(var_history) }
 
       define_method("#{name}=".to_sym) do |value|
+        history << instance_variable_get(var_name)
+
         instance_variable_set(var_name, value)
-        history << value
-        instance_variable_set(var_name_history, history.take(history.size - 1))
+        instance_variable_set(var_history, history)
       end
     end
   end
