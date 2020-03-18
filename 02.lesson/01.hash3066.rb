1e47da914805a2b903bc045532d47a55af11fba4
enextus <enextus@gmail.com>
2020-03-18 08:37:18 +0100

    cla

---
+++ 02.lesson/01.hash3066.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Specific days search program
+def months
+  hash = { January: 31, February: 28, March: 31, April: 30,
+           May: 31, June: 30, July: 31, August: 31,
+           September: 30, October: 31, November: 30, December: 31 }
+  hash.each do |month, day|
+    puts "#{month}: #{day}" if day == 30 or day == 28
+  end
+end
+months
